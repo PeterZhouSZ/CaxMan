68867270d0b63c2a358ecd136298ae45ddb13164
cino <cino.83@gmail.com>
2016-07-14 14:09:55 +0200

    orientation service

---
+++ orientation_service/main.cpp
@@ -0,0 +1,23 @@
+#include <caxlib/process_plan/orient.h>
+#include <caxlib/trimesh/trimesh.h>
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        caxlib::logger << "Usage: ./orientation_service input.off output.ann" << caxlib::endl;
+        return 0;
+    }
+
+    double angle_thresh   = 30.0;
+    int    dirs_pool_size = 100;
+
+    caxlib::Trimesh m(argv[1]);
+    caxlib::orient(m, angle_thresh, dirs_pool_size);
+
+    m.save(argv[2]);
+
+    return 0;
+}
