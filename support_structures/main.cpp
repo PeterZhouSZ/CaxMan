b9b2bb60c89ef5993cda4f6b2ba95ce4b8c56795
marco.livesu@ge.imati.cnr.it <marco.livesu@ge.imati.cnr.it>
2017-04-06 18:37:10 +0200

    cinoservizi in

---
+++ support_structures/main.cpp
@@ -0,0 +1,20 @@
+#include "caxlib/process_plan/support_structures.h"
+#include <trimesh/trimesh.h>
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        caxlib::logger << "Usage: ./supports_service input.zip output.zip" << caxlib::endl;
+        return 0;
+    }
+
+    caxlib::Trimesh m(argv[1]);
+    caxlib::create_support_structures(m);
+
+    m.save(argv[2]);
+
+    return 0;
+}
